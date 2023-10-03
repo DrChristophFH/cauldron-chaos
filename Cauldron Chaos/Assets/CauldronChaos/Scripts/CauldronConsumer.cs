0c5ade147bad9e1c36937626b60bddeb16e31c90
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-10-03 23:13:53 +0200

    Add cauldron content

--- Cauldron Chaos/Assets/CauldronChaos/Scripts/CauldronConsumer.cs
+++ Cauldron Chaos/Assets/CauldronChaos/Scripts/CauldronConsumer.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class CauldronConsumer : MonoBehaviour {
   [SerializeField]
   private Cauldron cauldron;
+  [SerializeField]
+  private GameObject dropParticles;
 
   private void OnTriggerEnter(Collider other) {
     if (other.TryGetComponent(out Ingredient ingredient)) {
       foreach (Part part in ingredient.Parts) {
         Debug.Log($" - Adding {part.Amount} {part.Material.name} to cauldron from {ingredient.Name}");
         cauldron.AddPart(part);
+        Destroy(other.gameObject);
+
+        Instantiate(dropParticles, other.transform.position, Quaternion.identity);
       }
     }
   }
